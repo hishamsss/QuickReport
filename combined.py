e79fdc5c86aff7fdcce3e911184c825959063ca9
hishamsss <97560247+hishamsss@users.noreply.github.com>
2025-05-01 07:54:55 +0400

    Update combined.py

--- combined.py
+++ combined.py
@@ -295,14 +295,14 @@ if uploaded_doc and uploaded_wisc:
                 df.columns = df.iloc[0]
                 df = df.drop(index=0).reset_index(drop=True)
 
-                #st.write(f"📊 Raw WISC Table {i+1}")
-                #st.dataframe(df)
+                st.write(f"📊 Raw WISC Table {i+1}")
+                st.dataframe(df)
 
                 if df.shape[1] >= 6:
                     ae_df = df.iloc[:, [1, 5]].copy()
                     ae_df.columns = ['Name', 'Percentile']
-                    #st.write(f"🧠 A/E Data from WISC Table {i+1}")
-                    #st.dataframe(ae_df)
+                    st.write(f"🧠 A/E Data from WISC Table {i+1}")
+                    st.dataframe(ae_df)
             
                     wisc_combined = pd.concat([wisc_combined, ae_df], ignore_index=True)
 
